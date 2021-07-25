3e76d4aecf37ba16ac9d1920e43a577da9134cfa
Devfarley <66633806+Devfarley@users.noreply.github.com>
2021-07-24 21:11:02 -0600

    React Core Concepts

---
+++ src/components/common/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <a href='/'>Home</a> | <a href='/about'>About</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
