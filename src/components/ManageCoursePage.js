47979b57e0ae6cefcea5bca38f45629aa564ad2a
Devfarley <66633806+Devfarley@users.noreply.github.com>
2021-08-10 14:31:16 -0600

    forms

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CourseForm from './CourseForm';
+import * as courseApi from '../api/courseApi';
+import { toast } from 'react-toastify';
 
 const ManageCoursePage = (props) => {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: '',
@@ -9,10 +12,51 @@ const ManageCoursePage = (props) => {
     authorId: null,
     category: '',
   });
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then((_course) => setCourse(_course));
+    }
+  }, [props.match.params.slug]);
+
+  const handleChange = ({ target }) => {
+    setCourse({
+      ...course,
+      [target.name]: target.value,
+    });
+  };
+
+  const formIsValid = () => {
+    const _errors = {};
+
+    if (!course.title) _errors.title = 'Title is required';
+    if (!course.authorId) _errors.authorId = 'Author Id is required';
+    if (!course.category) _errors.category = 'Category is required';
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!formIsValid()) return;
+    courseApi.saveCourse(course).then(() => {
+      props.history.push('/courses');
+      toast.success('Course Saved.');
+    });
+  };
+
   return (
     <>
       <h2>Manage Course</h2>
-      <CourseForm course={course}></CourseForm>
+      <CourseForm
+        errors={errors}
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      ></CourseForm>
     </>
   );
 };
