e8e75872bd25828cfb08864623785832c54ec67e
Devfarley <66633806+Devfarley@users.noreply.github.com>
2021-08-11 18:33:24 -0600

    flux basic actions

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import CourseForm from './CourseForm';
-import * as courseApi from '../api/courseApi';
+import courseStore from '../stores/courseStore';
+import * as courseActions from '../actions/courseActions';
 import { toast } from 'react-toastify';
 
 const ManageCoursePage = (props) => {
@@ -16,9 +17,9 @@ const ManageCoursePage = (props) => {
   useEffect(() => {
     const slug = props.match.params.slug;
     if (slug) {
-      courseApi.getCourseBySlug(slug).then((_course) => setCourse(_course));
+      setCourse(courseStore.getCourseBySlug(slug));
     }
-  }, [props.match.params.slug]);
+  }, [props.match.params]);
 
   const handleChange = ({ target }) => {
     setCourse({
@@ -42,7 +43,7 @@ const ManageCoursePage = (props) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     if (!formIsValid()) return;
-    courseApi.saveCourse(course).then(() => {
+    courseActions.saveCourse(course).then(() => {
       props.history.push('/courses');
       toast.success('Course Saved.');
     });
